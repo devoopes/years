40e2e0f5140c38c3b533b9c23d6eee8ad71f9228
Sean McCabe <sean@ulation.com>
2021-08-02 10:27:20 -0700

    Added changes from PR and cleanup.

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,43 +1,31 @@
 #!/bin/bash
 
+year=2020
+playlist="https://www.youtube.com/playlist?list=PLEVVO3zRKfv_XK06fOi41FQVATxgOHs6o"
+source_directory="2020Lockout"
+dest_directory=$year
+sourcefile="evernote-list.txt"
+destfile="final-list.txt"
 # Paylists
 # 2019: https://www.youtube.com/playlist?list=PLhEVvej0BBEI3YMccR3_CLDiBSNvGFv5I
 # 2002: https://www.youtube.com/playlist?list=PLEVVO3zRKfv_XK06fOi41FQVATxgOHs6o
 
-year=2020
-playlist="https://www.youtube.com/playlist?list=PLEVVO3zRKfv_XK06fOi41FQVATxgOHs6o"
-source_directory="2020Lockout"
-dest_directory="2020"
-
-### Santatize Data Input ###
+# Sanitize Data Input
 echo -n "Before starting, copy list from Evernote to evernote-list.txt"
 pause
 
 echo -n "Removing all lines that start with #'s"
 # Remove all lines that start with #
+sed '/^#.*\n/d' $sourcefile > $destfile
+# Replace all " - " with ","'s'
+sed ' - /d' $destfile > "music/$year.csv"
+# Remove all Quotation Marks
 while read input; do
-  sed '/^#.*/d' "$input"
-done <evernote-list.txt
-cp evernote-list.txt final-list.txt
-
-# replace all " - " with ","'s'
-while read input; do
-  sed '/^#.*/d' "$input"
-done <final-list.txt > years/$year.csv
-
-#TODO ##############################
-# remove all quotation marks
-
-
-
-
-while read input; do
-  sed  's/ - /,/g' "$input"
   sed 's/\"//g' "$input"
   sed "s/\'//g" "$input"
-done <input.txt
+done <"music/$year.csv"
 
-### Youtube Download ###
+# Youtube Download
 echo -n "Upgrade youtube-dl? (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
@@ -52,18 +40,16 @@ else
   printf "Skipping Homebrew Update"
 fi
 
-pause
-
+wait 2
 youtube-dl -i  -c --download-archive downloaded.txt \
-      --extract-audio \
-      --audio-format mp3 \
-      --audio-quality 0 \
-      --default-search "ytsearch" \
-      -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' \
-      --no-overwrites \
-      --yes-playlist $playlist
-
-      #--output "2020/%(title)s.%(ext)s" \
+  --extract-audio \
+  --audio-format mp3 \
+  --audio-quality 0 \
+  --default-search "ytsearch" \
+  -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' \
+  --no-overwrites \
+  --yes-playlist $playlist
+  #--output "2020/%(title)s.%(ext)s" \
 
 # Check Directory Counts Match
 echo -n "Check directory count vs count in final-list.txt"
@@ -74,32 +60,35 @@ echo -n "Directory list count="$dirlist
 if (( $finallist = $dirlist ))
 then
   echo -n "The counts match."
+  diff $finallist $dirlist
 else
   echo -n "The counts do not match."
-  exit
+  diff $finallist $dirlist
+  exit 1
 fi
 
 # Rename files
 for file in ./$source_directory/*.mp3;do
-       read line
-       cp -v "${file}" "$dest_directory/${line}.mp3" >> rename.log
-done < final-list.txt
+   read line
+   cp -v "${file}" "$dest_directory/${line}.mp3" >> rename.log
+done < $destfile.txt
 
+# Cleanup
 echo -n "Move folder to Dropbox? (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-  mv $year ~/Dropbox/music
+  mv $year ~/Dropbox/music/
 else
   printf "Skipping"
 fi
 
+wait 2
 echo -n "Cleanup? (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
   rm -y downloaded.txt
   rm -y final-list.txt
   rm -y ./*.zip
-  rm -rf ./$year*
 else
   printf "Skipping"
 fi
